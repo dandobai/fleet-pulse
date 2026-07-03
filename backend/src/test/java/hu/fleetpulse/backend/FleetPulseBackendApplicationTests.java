633f952196e525dcfd617c51391e392a3f823ec5
Daniel Dobai <dandobai@gmail.com>
2026-07-03 02:42:08 +0200

    chore: initial spring boot application structure

---
+++ backend/src/test/java/hu/fleetpulse/backend/FleetPulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.fleetpulse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetPulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
