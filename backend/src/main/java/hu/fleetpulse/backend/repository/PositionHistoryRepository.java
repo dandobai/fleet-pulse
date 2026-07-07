c594f45f0cc8dd19a638eaa9eb76c9da1e921565
Daniel Dobai <dandobai@gmail.com>
2026-07-07 10:42:34 +0200

    update specify param for the findByVehicle, also changing method name from findAllBy

--- backend/src/main/java/hu/fleetpulse/backend/repository/PositionHistoryRepository.java
+++ backend/src/main/java/hu/fleetpulse/backend/repository/PositionHistoryRepository.java
@@ -2,10 +2,11 @@ package hu.fleetpulse.backend.repository;
 
 import hu.fleetpulse.backend.model.entity.PositionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface PositionHistoryRepository extends JpaRepository<PositionHistory, UUID> {
-    List<PositionHistory> findAllByVehicleId(UUID vehicleId);
-}
+    List<PositionHistory> findByVehicleId(@Param("vehicle_id") UUID vehicleId);}
