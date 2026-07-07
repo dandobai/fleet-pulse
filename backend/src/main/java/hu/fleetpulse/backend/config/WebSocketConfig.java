9f55d7b5adcd47ce1a571c6e670319b284e4d369
Daniel Dobai <dandobai@gmail.com>
2026-07-07 10:37:51 +0200

    update: websocket config from hardcoded urls to refferences

--- backend/src/main/java/hu/fleetpulse/backend/config/WebSocketConfig.java
+++ backend/src/main/java/hu/fleetpulse/backend/config/WebSocketConfig.java
@@ -23,7 +23,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-fleet")
-                .setAllowedOrigins(allowedOrigins)
-                .withSockJS();
+                .setAllowedOrigins(allowedOrigins);
     }
 }
